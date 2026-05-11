05991a44a550e0b96ad1ed3d98125015fce7ad3a
simonegreco5 <simoxey.greco@gmail.com>
2026-05-11 17:50:57 +0200

    - add bootstrap - add update film list structure in AppMain

--- src/assets/array.js
+++ src/assets/array.js
@@ -1,29 +1,35 @@
  
- const films = [
+ const filmList = [
     {
+        id: 1,
         title: 'Inception',
         genere: 'Fantascienza' 
     },
-    { 
+    {
+        id: 2, 
         title: 'Il Padrino', 
         genere: 'Thriller' 
     },
-    { 
+    {
+        id: 3, 
         title: 'Titanic', 
         genere: 'Romantico' 
     },
-    { 
+    {
+        id: 4, 
         title: 'Batman', 
         genere: 'Azione' 
     },
-    { 
+    {
+        id: 5, 
         title: 'Interstellar', 
         genere: 'Fantascienza' 
     },
-    { 
+    {
+        id: 6, 
         title: 'Pulp Fiction', 
         genere: 'Thriller' 
     },
  ]
  
- export default films
+ export default filmList
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,10 +1,33 @@
 // sezione import
+import { useState } from "react"
+import filmList from "../assets/array.js"
 
 export default function AppMain() {
 
+    const [films, setFilms] = useState(filmList)
+
     return (
         <main>
-
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Film</th>
+                        <th scope="col">Genere</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        films.map((item) => (
+                            <tr key={item.id}>
+                                <th scope="row">{item.id}</th>
+                                <td>{item.title}</td>
+                                <td>{item.genere}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </main>
     )
 }
