350ee5ca88c6c2a0084694f138ab3aa995c17050
simonegreco5 <simoxey.greco@gmail.com>
2026-05-11 18:01:42 +0200

    - add fix

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -8,26 +8,33 @@ export default function AppMain() {
 
     return (
         <main>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Film</th>
-                        <th scope="col">Genere</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        films.map((item) => (
-                            <tr key={item.id}>
-                                <th scope="row">{item.id}</th>
-                                <td>{item.title}</td>
-                                <td>{item.genere}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+            <div className="container">
+
+                {/* filtraggio - bottone con dropdown */}
+                
+
+                {/* tabella film */}
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Film</th>
+                            <th scope="col">Genere</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            films.map((item) => (
+                                <tr key={item.id}>
+                                    <th scope="row">{item.id}</th>
+                                    <td>{item.title}</td>
+                                    <td>{item.genere}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </main>
     )
 }
