b8b7c9a106e512a30a150eb75f9c205f438ce420
simonegreco5 <simoxey.greco@gmail.com>
2026-05-12 00:27:05 +0200

    - add film filtering in useEffect - add fix

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,17 +1,34 @@
 // sezione import
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import filmList from "../assets/array.js"
 
 export default function AppMain() {
 
     const [films, setFilms] = useState(filmList)
+    const [filtro, setFiltro] = useState(" ")
+
+    useEffect(() => {
+        if (filtro === " ") {
+            setFilms(filmList);
+        } else {
+            const filteredFilm = filmList.filter((film) => film.genere === filtro );
+            setFilms(filteredFilm);
+        }
+    }, [filtro])
 
     return (
         <main>
             <div className="container">
 
                 {/* filtraggio - bottone con dropdown o imput con search */}
-                
+                <select onChange={(e) => setFiltro(e.target.value)} className="form-select">
+                    <option selected>scegli genere film</option>
+                    <option value=" ">Tutti</option>
+                    <option value="Fantascienza">Fantascienza</option>
+                    <option value="Thriller">Thriller</option>
+                    <option value="Romantico">Romantico</option>
+                    <option value="Azione">Azione</option>
+                </select>
 
                 {/* tabella film */}
                 <table className="table">
